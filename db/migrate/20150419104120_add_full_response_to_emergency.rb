6ad174570b7e71d3d02c095db9cc3de505f4279c
alexlag <bo4onok@gmail.com>
2015-04-22 02:01:33 +0300

    Index for full_response in Emergency

--- db/migrate/20150419104120_add_full_response_to_emergency.rb
+++ db/migrate/20150419104120_add_full_response_to_emergency.rb
@@ -1,5 +1,5 @@
 class AddFullResponseToEmergency < ActiveRecord::Migration
   def change
-    add_column :emergencies, :full_response, :boolean, default: false
+    add_column :emergencies, :full_response, :boolean, index: true, default: false
   end
 end
